5471b6d3fd5372aa91e28c372eaf84c1a111fca0
David Höppner <0xffea@gmail.com>
2014-11-12 11:11:12 +0100

    Use random names in test.ml

--- test/test.ml
+++ test/test.ml
@@ -14,6 +14,11 @@ let redis_test_port () =
   with Not_found ->
     6379
 
+let redis_string_bucket() =
+  let number = Random.bits () in
+  let bucket = ("ounit_" ^ string_of_int(number)) in
+    bucket
+
 let redis_open_connection =
   let host = redis_test_host () in
   let port = redis_test_port () in
@@ -25,15 +30,27 @@ let setup _ =
 let teardown conn =
   ()
 
+(* PING *)
 let test_case_ping conn =
   match Redis_sync.Client.ping conn with
     | true -> ()
     | false -> assert_failure("Can't connect to Redis server")
 
+(* ECHO *)
 let test_case_echo conn =
   match Redis_sync.Client.echo conn "ECHO" with
     | Some "ECHO" -> ()
     | _ -> assert_failure("Can't echo to Redis server")
+
+(* SET *)
+(* GET *)
+let test_case_set_string conn =
+  let key = redis_string_bucket() in
+  let value = redis_string_bucket() in
+  let err = Redis_sync.Client.set conn key value in
+  let result = Redis_sync.Client.get conn key in
+  assert_bool "Can't set key" (err = ()) ;
+  assert_bool "Key and value mismatch" (result = Some value)
 
 let bracket test_case () =
   let conn = setup () in
@@ -44,5 +61,7 @@ let _ =
   let suite = "Redis" >::: [
     "test_case_ping" >:: (bracket test_case_ping);
     "test_case_echo" >:: (bracket test_case_echo);
+    "test_case_set_string" >:: (bracket test_case_set_string);
   ] in
+  Random.self_init () ;
   run_test_tt_main suite
