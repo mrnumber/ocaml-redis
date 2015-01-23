2a840d7cb9ff701c136f6ace829120ab842db05b
aluuu <aluuu@husa.su>
2015-01-23 17:01:37 +0500

    fix the way assertions are combined, make them sequential as we expect

--- test/test.ml
+++ test/test.ml
@@ -69,49 +69,47 @@ module Make(IO : Redis.Make.IO) = struct
 
   (* INFO *)
   let test_case_info conn =
-    Client.info conn
-    >>= fun result ->
-    IO.return
-      (let tcp_port = List.filter (fun (k, v) -> k = "tcp_port") result
-                      |> List.hd |> snd in
-       assert_bool "Got wrong data about port with INFO command"
-                   (int_of_string tcp_port = redis_test_port()))
+    Client.info conn >>=
+      fun result ->
+      IO.return
+        (let tcp_port = List.filter (fun (k, v) -> k = "tcp_port") result
+                        |> List.hd |> snd in
+         assert_bool "Got wrong data about port with INFO command"
+                     (int_of_string tcp_port = redis_test_port()))
 
   (* Keys test case *)
   let test_case_keys conn =
     let key = redis_string_bucket () in
     let value = redis_string_bucket () in
+    let key' = redis_string_bucket () in
+    let key'' = redis_string_bucket () in
+
     Client.set conn key value >>=
-      io_assert "Can't set key" ((=) ());
+      io_assert "Can't set key" ((=) ()) >>= fun () ->
     Client.get conn key >>=
-      io_assert "Key and value mismatch" ((=) (Some value));
+      io_assert "Key and value mismatch" ((=) (Some value)) >>= fun () ->
     Client.exists conn key >>=
-      io_assert "Key doesn't exist" ((=) true);
+      io_assert "Key doesn't exist" ((=) true) >>= fun () ->
     Client.keys conn key >>=
       io_assert "Can't find with itself as a pattern in KEYS command"
                 (fun keys -> let found_key = List.find (fun k -> k = key) keys in
-                             (found_key = key));
+                             (found_key = key)) >>= fun () ->
     Client.randomkey conn >>=
-      io_assert "Can't find key with RANDOMKEY command" ((<>) None);
+      io_assert "Can't find key with RANDOMKEY command" ((<>) None) >>= fun () ->
     Client.move conn key 2 >>=
-      io_assert "Can't move key to redis database #2" ((=) true);
-
+      io_assert "Can't move key to redis database #2" ((=) true) >>= fun () ->
     Client.select conn 2 >>=
-      io_assert "Can't select redis database #2" ((=) ());
-
-    let key' = redis_string_bucket () in
+      io_assert "Can't select redis database #2" ((=) ()) >>= fun () ->
     Client.rename conn key key' >>=
-      io_assert "Can't rename key" ((=) ());
-
-    let key'' = redis_string_bucket () in
+      io_assert "Can't rename key" ((=) ()) >>= fun () ->
     Client.set conn key'' value >>=
-      io_assert "Can't set key''" ((=) ());
+      io_assert "Can't set key''" ((=) ()) >>= fun () ->
     Client.renamenx conn key' key'' >>=
-      io_assert "Can renamenx key" ((=) false);
+      io_assert "Can renamenx key" ((=) false) >>= fun () ->
     Client.rename conn key' key >>=
-      io_assert "Can't rename key" ((=) ());
+      io_assert "Can't rename key" ((=) ()) >>= fun () ->
     Client.del conn [key; key''] >>=
-      io_assert "Key wasn't deleted" ((=) 2);
+      io_assert "Key wasn't deleted" ((=) 2) >>= fun () ->
     Client.select conn 0 >>=
       io_assert "Can't select redis database #0" ((=) ())
 
@@ -120,7 +118,7 @@ module Make(IO : Redis.Make.IO) = struct
     let values = List.rev keys in
     let kv_pairs = List.combine keys values in
     Client.mset conn kv_pairs >>=
-      io_assert "Can't set multiple keys" ((=) ());
+      io_assert "Can't set multiple keys" ((=) ()) >>= fun () ->
 
     let expected_values = List.map (fun x -> Some x) values in
     Client.mget conn keys >>=
@@ -128,12 +126,12 @@ module Make(IO : Redis.Make.IO) = struct
        IO.return
          (List.iter2
             (fun expected actual -> assert_bool "Got unexpected value" (expected = actual))
-            expected_values actual_values));
+            expected_values actual_values)) >>= fun () ->
 
     let another_values = redis_n_strings_bucket 10 in
     let kv_pairs = List.combine keys another_values in
     Client.msetnx conn kv_pairs >>=
-      io_assert "It's possible MSETNX multiple keys" ((=) false);
+      io_assert "It's possible MSETNX multiple keys" ((=) false) >>= fun () ->
 
     let another_keys = redis_n_strings_bucket 10 in
     let kv_pairs = List.combine another_keys another_values in
@@ -144,42 +142,39 @@ module Make(IO : Redis.Make.IO) = struct
     let key = redis_string_bucket () in
     let value = redis_string_bucket () in
     Client.set conn key value >>=
-      io_assert "Can't set key" ((=) ());
+      io_assert "Can't set key" ((=) ()) >>= fun () ->
     Client.dump conn key >>=
-      io_assert "Can't dump value"
-                (function
-                  | None -> false
-                  | Some value_dump ->
-                     let key' = String.concat "" [key; redis_string_bucket ()] in
-                     Client.restore conn key' 0 value_dump >>=
-                       io_assert "Can't restore value" ((=) ());
-                     Client.get conn key' >>=
-                       io_assert "Key value and restored value mismatch" ((=) (Some value));
-                     true)
+      function
+      | None -> assert_failure "Can't dump value"
+      | Some value_dump ->
+         let key' = String.concat "" [key; redis_string_bucket ()] in
+         Client.restore conn key' 0 value_dump >>=
+           io_assert "Can't restore value" ((=) ()) >>= fun () ->
+         Client.get conn key' >>=
+           io_assert "Key value and restored value mismatch" ((=) (Some value))
 
   let test_case_expire conn =
     let key = redis_string_bucket () in
     let value = redis_string_bucket () in
     Client.set conn key value >>=
-      io_assert "Can't set key" ((=) ());
+      io_assert "Can't set key" ((=) ()) >>= fun () ->
     Client.expire conn key 1 >>=
-      io_assert "Can't set expiration timeout for key" ((=) true);
+      io_assert "Can't set expiration timeout for key" ((=) true) >>= fun () ->
     Client.pexpire conn key 1000 >>=
-      io_assert "Can't set expiration timeout in milliseconds for key" ((=) true);
+      io_assert "Can't set expiration timeout in milliseconds for key" ((=) true) >>= fun () ->
     Client.ttl conn key >>=
       io_assert "Can't check expiration timeout for key"
-                (fun x -> List.mem x [Some 0; Some 1]);
+                (fun x -> List.mem x [Some 0; Some 1]) >>= fun () ->
     Client.pttl conn key >>=
-      io_assert "Can't get correct timeout for key"
-                (function
-                  | Some pttl ->
-                     assert_bool "Expiration timeout differs from setted" (0 <= pttl && pttl <= 1000);
-                     true
-                  | None ->
-                     assert_failure "Can't check expiration timeout for key";
-                     false);
+      (function
+        | Some pttl ->
+           assert_bool "Expiration timeout differs from setted" (0 <= pttl && pttl <= 1000);
+           IO.return ()
+        | None ->
+           assert_failure "Can't check expiration timeout for key";
+           IO.return ()) >>= fun () ->
     Client.persist conn key >>=
-      io_assert "Can't remove existing timeout on key" ((=) true);
+      io_assert "Can't remove existing timeout on key" ((=) true)  >>= fun () ->
     Client.ttl conn key >>=
       io_assert "Can't check expiration timeout for key" ((=) None)
 
@@ -187,39 +182,38 @@ module Make(IO : Redis.Make.IO) = struct
     let key = redis_string_bucket () in
     let value = redis_string_bucket () in
     Client.set conn key value >>=
-      io_assert "Can't set key" ((=) ());
+      io_assert "Can't set key" ((=) ()) >>= fun () ->
     let expiry = Unix.time () +. 1. in
     Client.expireat conn key expiry >>=
-      io_assert "Can't set expiration timeout for key" ((=) true);
+      io_assert "Can't set expiration timeout for key" ((=) true) >>= fun () ->
     Client.ttl conn key >>=
       io_assert "Can't check expiration timeout for key"
-                (fun x -> List.mem x [Some 0; Some 1]);
+                (fun x -> List.mem x [Some 0; Some 1]) >>= fun () ->
 
     let pexpiry = int_of_float (Unix.time ()) * 1000 + 1000 in
     Client.pexpireat conn key pexpiry >>=
-      io_assert "Can't set expiration timeout for key (in ms)" ((=) true);
+      io_assert "Can't set expiration timeout for key (in ms)" ((=) true) >>= fun () ->
     Client.pttl conn key >>=
-      io_assert "Can't get correct timeout for key"
-                (function
-                  | Some pttl ->
-                     assert_bool "Expiration timeout differs from setted" (0 <= pttl && pttl <= 1000);
-                     true
-                  | None ->
-                     assert_failure "Can't check expiration timeout for key";
-                     false)
+      (function
+        | Some pttl ->
+           assert_bool "Expiration timeout differs from setted" (0 <= pttl && pttl <= 1000);
+           IO.return ()
+        | None ->
+           assert_failure "Can't check expiration timeout for key";
+           IO.return ())
 
   let test_case_type conn =
     let value = redis_string_bucket () in
     let string_key = redis_string_bucket () in
     Client.set conn string_key value >>=
-      io_assert "Can't set key" ((=) ());
+      io_assert "Can't set key" ((=) ()) >>= fun () ->
 
     let list_key = redis_string_bucket () in
     Client.lpush conn list_key value >>=
-      io_assert "Can't push value to list" ((=) 1);
+      io_assert "Can't push value to list" ((=) 1) >>= fun () ->
 
     Client.type_of conn string_key >>=
-      io_assert "Got wrong key type for string_key" ((=) `String);
+      io_assert "Got wrong key type for string_key" ((=) `String) >>= fun () ->
     Client.type_of conn list_key >>=
       io_assert "Got wrong key type for list_key" ((=) `List)
 
@@ -229,10 +223,10 @@ module Make(IO : Redis.Make.IO) = struct
     let value = redis_string_bucket () in
     Client.append conn key value >>=
       io_assert "Can't append initial value to key"
-                (fun x -> x = String.length value);
+                (fun x -> x = String.length value) >>= fun () ->
     Client.append conn key value >>=
       io_assert "Can't append additional value to key"
-                (fun x -> x = (String.length value + String.length value));
+                (fun x -> x = (String.length value + String.length value)) >>= fun () ->
     Client.get conn key >>=
       io_assert "Can't get key" ((=) (Some (String.concat "" [value; value])))
 
@@ -242,18 +236,18 @@ module Make(IO : Redis.Make.IO) = struct
     let value = redis_integer_bucket () in
     let increment = redis_integer_bucket () in
     Client.set conn key (string_of_int value) >>=
-      io_assert "Can't set float value to key" ((=) ());
+      io_assert "Can't set float value to key" ((=) ()) >>= fun () ->
     Client.incrby conn key increment >>=
-      io_assert "Can't increment value by integer" ((=) (value + increment));
+      io_assert "Can't increment value by integer" ((=) (value + increment)) >>= fun () ->
     Client.incr conn key >>=
-      io_assert "Can't increment value by one" ((=) (value + increment + 1));
+      io_assert "Can't increment value by one" ((=) (value + increment + 1)) >>= fun () ->
     Client.decrby conn key increment >>=
-      io_assert "Can't decrement value by integer" ((=) (value + 1));
+      io_assert "Can't decrement value by integer" ((=) (value + 1)) >>= fun () ->
     Client.decr conn key >>=
-      io_assert "Can't decrement value by one" ((=) value);
+      io_assert "Can't decrement value by one" ((=) value) >>= fun () ->
     Client.incrbyfloat conn key 2. >>=
       io_assert "Can't increment value by float"
-                ((=) (float_of_int value +. 2.));
+                ((=) (float_of_int value +. 2.)) >>= fun () ->
     Client.incrbyfloat conn key (- 2.) >>=
       io_assert "Can't increment value by negative float"
                 ((=) (float_of_int value))
@@ -267,64 +261,63 @@ module Make(IO : Redis.Make.IO) = struct
     let value2 = "abcdef" in
     let value3 = "\x00\xff\xf0" in
     Client.set conn key1 value1 >>=
-      io_assert "Can't set value1 to key1" ((=) ());
+      io_assert "Can't set value1 to key1" ((=) ()) >>= fun () ->
     Client.set conn key2 value2 >>=
-      io_assert "Can't set value2 to key2" ((=) ());
+      io_assert "Can't set value2 to key2" ((=) ()) >>= fun () ->
     Client.bitop conn Client.AND dest [key1; key2] >>=
-      io_assert "Can't execute BITOP AND key1 and key2" ((=) 6);
+      io_assert "Can't execute BITOP AND key1 and key2" ((=) 6) >>= fun () ->
     Client.get conn dest >>=
-      io_assert "Got unexpected value from dest" ((=) (Some "`bc`ab"));
+      io_assert "Got unexpected value from dest" ((=) (Some "`bc`ab")) >>= fun () ->
     Client.bitop conn Client.NOT dest [key1] >>=
-      io_assert "Can't execute BITOP NOT key1" ((=) 6);
+      io_assert "Can't execute BITOP NOT key1" ((=) 6) >>= fun () ->
     Client.get conn dest >>=
-      io_assert "Got unexpected value from dest" ((=) (Some "\x99\x90\x90\x9d\x9e\x8d"));
+      io_assert "Got unexpected value from dest" ((=) (Some "\x99\x90\x90\x9d\x9e\x8d")) >>= fun () ->
     Client.set conn key1 value3 >>=
-      io_assert "Can't set value3 to key1" ((=) ());
+      io_assert "Can't set value3 to key1" ((=) ()) >>= fun () ->
     Client.bitpos conn key1 1 >>=
-      io_assert "Got unexpected bit position" ((=) 8);
+      io_assert "Got unexpected bit position" ((=) 8) >>= fun () ->
     Client.bitpos conn key1 1 ~first:0 >>=
-      io_assert "Got unexpected bit position" ((=) 8);
+      io_assert "Got unexpected bit position" ((=) 8) >>= fun () ->
     Client.bitpos conn key1 1 ~first:2 >>=
-      io_assert "Got unexpected bit position" ((=) 16);
+      io_assert "Got unexpected bit position" ((=) 16) >>= fun () ->
     Client.getbit conn key1 0 >>=
-      io_assert "Can't get bit" ((=) 0);
+      io_assert "Can't get bit" ((=) 0) >>= fun () ->
     Client.setbit conn key1 0 1 >>=
-      io_assert "Can't set bit" ((=) 0);
+      io_assert "Can't set bit" ((=) 0) >>= fun () ->
     Client.getbit conn key1 0 >>=
-      io_assert "Can't get bit" ((=) 1);
+      io_assert "Can't get bit" ((=) 1) >>= fun () ->
     Client.set conn key1 value1 >>=
-      io_assert "Can't set value1 to key1" ((=) ());
+      io_assert "Can't set value1 to key1" ((=) ()) >>= fun () ->
     Client.bitcount conn key1 >>=
-      io_assert "Got unexpected bit count" ((=) 26);
+      io_assert "Got unexpected bit count" ((=) 26) >>= fun () ->
     Client.bitcount conn key1 ~first:1 >>=
-      io_assert "Got unexpected bit count" ((=) 22);
+      io_assert "Got unexpected bit count" ((=) 22) >>= fun () ->
     Client.bitcount conn key1 ~first:0 ~last:0 >>=
-      io_assert "Got unexpected bit count" ((=) 4);
+      io_assert "Got unexpected bit count" ((=) 4) >>= fun () ->
     Client.bitcount conn key1 ~first:1 ~last:1 >>=
       io_assert "Got unexpected bit count" ((=) 6)
 
-  (* let test_case_scan conn = *)
-  (*   let rec scan_keys cursor keys = *)
-  (*     Client.scan conn cursor >>= *)
-  (*       (fun (next_cursor, next_keys) -> *)
-  (*        let next_keys = List.concat [keys; next_keys] in *)
-  (*        if next_cursor == 0 then *)
-  (*          IO.return next_keys *)
-  (*        else *)
-  (*          scan_keys next_cursor next_keys) in *)
-  (*   let scan_all_keys () = scan_keys 0 [] in *)
-  (*   Client.keys conn "*" >>= *)
-  (*     (fun keys -> *)
-  (*      scan_all_keys () >>= *)
-  (*        io_assert "Number of keys got with KEYS command is not equal to number of keys got with SCAN command" *)
-  (*                  (fun scanned_keys -> List.length keys = List.length scanned_keys)) *)
+  let test_case_scan conn =
+    let rec scan_keys cursor keys =
+      Client.scan conn cursor >>=
+        (fun (next_cursor, next_keys) ->
+         let next_keys = List.concat [keys; next_keys] in
+         if next_cursor == 0 then
+           IO.return next_keys
+         else
+           scan_keys next_cursor next_keys) in
+    let scan_all_keys () = scan_keys 0 [] in
+    Client.keys conn "*" >>=
+      (fun keys ->
+       scan_all_keys () >>=
+         io_assert "Number of keys got with KEYS command is not equal to number of keys got with SCAN command"
+                   (fun scanned_keys -> List.length keys = List.length scanned_keys))
 
   let bracket test_case () =
     IO.run
-      (setup () >>=
-         (fun conn ->
-          test_case conn;
-          teardown conn))
+      (setup () >>= fun conn ->
+       test_case conn >>= fun () ->
+       teardown conn)
 
   let test () =
     let suite = "Redis" >::: [
@@ -340,7 +333,7 @@ module Make(IO : Redis.Make.IO) = struct
                 "test_case_append" >:: (bracket test_case_append);
                 "test_case_incr_decr" >:: (bracket test_case_incr_decr);
                 "test_case_bit_operations" >:: (bracket test_case_bit_operations);
-                (* "test_case_scan" >:: (bracket test_case_scan); *)
+                "test_case_scan" >:: (bracket test_case_scan);
               ] in
     Random.self_init ();
     run_test_tt_main suite
