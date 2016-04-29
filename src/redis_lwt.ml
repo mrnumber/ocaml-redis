3b931afdb1dfc383e89c9751aee3df530ec8d387
Alexander Dinu <aluuu@husa.su>
2016-04-29 15:57:02 +0300

    Change the way lwt client is connecting to redis
    Why:
    
    We should wait for `Lwt_unix.connect` call to finish before returning
    the file descriptor.

--- src/redis_lwt.ml
+++ src/redis_lwt.ml
@@ -19,11 +19,11 @@ module IO = struct
   let connect host port =
     let fd = Lwt_unix.socket (Unix.PF_INET) Unix.SOCK_STREAM 0 in
     let port = string_of_int port in
-    Lwt_unix.getaddrinfo host port [] >>= (function
-        | [] -> failwith "Could not resolve redis host!"
-        | addrinfo::_ -> return addrinfo.Lwt_unix.ai_addr) >>= (fun sock_addr ->
-        ignore (Lwt_unix.connect fd sock_addr);
-        return fd)
+    Lwt_unix.getaddrinfo host port [] >>= function
+    | [] -> failwith "Could not resolve redis host!"
+    | addrinfo::_ -> return addrinfo.Lwt_unix.ai_addr >>= fun sock_addr ->
+    Lwt_unix.connect fd sock_addr >>= fun () ->
+    return fd
 
   let close = Lwt_unix.close
   let sleep = Lwt_unix.sleep
