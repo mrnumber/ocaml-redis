9e5f48bb4c52837550d7329ccd2b46fad6c6f912
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-09-13 22:32:51 -0500

    test: add benchmark example, a merge sort via redis

--- examples/examples.ml
+++ examples/examples.ml
@@ -3,10 +3,12 @@ open Subscribe_lwt
 
 let host = ref "127.0.0.1"
 let port = ref 6379
+let n = ref 100_000
 
 let names = [
   "subscribe_sync", (fun () -> subscribe_sync !host !port);
   "subscribe_lwt", (fun () -> subscribe_lwt !host !port);
+  "bench_merge_sort", (fun () -> Bench_merge_sort.run ~n:!n !host !port);
 ]
 
 let () =
@@ -15,10 +17,10 @@ let () =
     "--name", Arg.Symbol (List.map fst names, (fun s -> name := s)), " pick example to run";
     "--host", Arg.Set_string host, " host to connect to";
     "--port", Arg.Set_int port, " port to connect to";
+    "-n", Arg.Set_int n, " size (for benchmarks)";
   ] |> Arg.align in
   Arg.parse opts (fun _ -> ()) "Example of usage ocaml-redis";
-  try
-    let f = List.assoc !name names in
-    f()
-  with _ ->
+  match List.assoc !name names with
+  | f -> f()
+  | exception _ ->
     failwith @@ "no such example: " ^ !name
