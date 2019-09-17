0a295b64497d2e4aeb1d37dfd777de660ad0838f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-09-16 20:56:21 -0500

    fix(api): export type of cache

--- src_lwt/redis_lwt.mli
+++ src_lwt/redis_lwt.mli
@@ -5,6 +5,7 @@ module Client : Redis.S.Client with module IO = IO
 module Cache (Params : Redis.S.Cache_params) : Redis.S.Cache
   with module IO = IO
   with module Client = Client
+  with module Params = Params
 
 module Mutex : Redis.S.Mutex
   with module IO = IO
